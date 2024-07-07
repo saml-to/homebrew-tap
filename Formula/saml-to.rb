1ec452f7a45df51e8fcb0244cc92804736acf902
Christian Nuss <cnuss@users.noreply.github.com>
2024-07-07 14:38:57 -0400

    saml-to@3.0.1

--- Formula/saml-to.rb
+++ Formula/saml-to.rb
@@ -3,8 +3,8 @@ require "language/node"
 class SamlTo < Formula
     desc "Command Line Interface for SAML.to"
     homepage "https://github.com/saml-to/cli"
-    url "https://github.com/saml-to/cli/archive/refs/tags/3.0.0.tar.gz"
-    sha256 "0b419b4cbb5f72b5fa94ab1789204327da1f26dd9c2e60596bb47196f8f1e15a"
+    url "https://github.com/saml-to/cli/archive/refs/tags/3.0.1.tar.gz"
+    sha256 "a519f7047e475a58cc969a33a741e52dbc0e1b7bbdf02b1586223725faca8069"
     license "Apache-2.0"
   
     depends_on "node"
